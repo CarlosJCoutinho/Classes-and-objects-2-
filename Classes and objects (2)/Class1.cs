0e5ef16583b1ab469271b1b2233e6b0a299a74d1
Carlos Coutinho <carlosj.coutinho@hotmail.com>
2020-05-20 23:32:53 +0100

    Add project files.

---
+++ Classes and objects (2)/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_objects__2_
+{
+    class Class1
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; set; }
+
+        public void BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+        }
+    }
+}
